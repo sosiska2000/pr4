9c2ff36a74575eb7b7bc24791d884c9f380bc04a
student-a502 <student-a502@STUD-RM11-A502>
2025-11-20 14:53:46 +0500

    Add project files with proper gitignore

---
+++ Common/FileInfoFTP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class FileInfoFTP
+    {
+        /// <summary> Массив байт
+        public byte[] Data { get; set; }
+        /// <summary> Имя файла
+        public string Name { get; set; }
+        /// <summary> Конструктор для заполнения класса
+        public FileInfoFTP(byte[] Data, string Name)
+        {
+            this.Data = Data;
+            this.Name = Name;
+        }
+    }
+}
---
+++ Common/Models/ViewModelSend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ViewModelSend
+    {
+        /// <summary> Сообщение отправляемое сервером
+        public string Message { get; set; }
+        /// <summary> Код пользователя
+        public int Id { get; set; }
+        /// <summary> Конструктор для заполнения класса
+        public ViewModelSend(string message, int id) 
+        {
+            this.Message = message;
+            this.Id = id;
+        }
+    }
+}
